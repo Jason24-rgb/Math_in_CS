8937653db454e49ab5d591bcbedd67ca0273dc38
Jason24-rgb <jason.tenelema63@gmail.com>
2025-07-04 21:38:16 -0400

    Adding math files

---
+++ Math/src/Mathematics.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Mathematics {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Matrix s1=new Matrix(2, 2);
+        double[] list={3,1,5,2}; //9.0,10.0,11,12,13,14,15,16
+        double[] ls2={2,-1,-5,3};
+        double[][] ls3={{1,2,3},{1,2,3}};
+        double[] ls4={1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4};
+        Matrix s2=new Matrix(2,2);
+        Matrix s3=new Matrix(ls3);
+        Matrix s4=new Matrix(4,4);
+        s1.assign(list);
+        s2.assign(ls2);
+        s4.assign(ls4);
+        Matrix transform= s4.t1(1,4);
+        /*String x3=s3.printx();
+        String x1=s1.printx();
+        String x2=s2.printx();
+        System.out.println(x1);
+        System.out.println(x3);
+        //System.out.println(Arrays.deepToString(s2.x));
+        System.out.println(x2);
+        System.out.println(s2.rows);*/
+        Matrix k= s1.multiply(s2);
+        String o=transform.printx();
+        System.out.println(o);
+
+    }   
+    
+
+}
---
+++ Math/src/Matrix.java
@@ -0,0 +1,131 @@
+import java.util.Arrays;
+
+public class Matrix {
+    double[][] x;
+    int cols;
+    int rows;
+    int totlel;
+    double[][] trans;
+
+    Matrix(int n, int m){
+        x=new double[n][m];
+        trans= new double[m][n];
+        rows=n;
+        cols=m;
+        totlel=m*n;
+    }
+    
+    Matrix(double[][] l){
+        rows=l.length;
+        cols=l[0].length;
+        x=l;
+    }
+
+    public String printx(){
+        String s="[";
+        for(int i=0;i<rows-1;i++){
+            s+=Arrays.toString(x[i])+",\n";
+        }
+        s+=Arrays.toString(x[rows-1]);
+        s=s+"]";
+        return s;
+    }
+
+    public void assign(double[] ls){      
+        int r=0;
+        int c=0;
+        if (ls.length>totlel){
+            System.out.println("total numbers has to be less thatn or equal to matrix length");
+            return;
+        }
+        
+        for (int i=0;i<ls.length;i++){
+            r=i/cols;
+            c=i%cols;
+            x[r][c]=ls[i];
+            trans[c][r]=ls[i];
+
+        }
+
+    }
+
+    public Matrix multiply(Matrix y){
+        if(this.cols!=y.rows){
+            System.out.println("num of rows of matrix A doesn't match num of columns of matrix B");
+            return null;
+        }
+
+        Matrix k= new Matrix(this.rows, y.cols);
+        int r1=0;
+        int r2=0;
+
+        //System.out.println(Arrays.deepToString(k.x));
+        // 1 loop 
+        for (int i=0;i<k.totlel;i++){
+            r1=i/y.cols;
+            r2=i%y.cols;
+            k.x[r1][r2]=dot(x[r1], y.trans[r2]);
+        }
+
+        // 2 loops
+        /*for(int i=0;i<k.rows;i++){
+            for(int j=0;j<k.cols;j++){
+                k.x[i][j]=dot(x[i], y.trans[j]);
+            }
+        }*/
+        
+
+
+
+        return k;
+    }
+
+    public Matrix t1(int r1, int r2){
+        r1-=1;
+        r2-=1;
+        double[][] a=this.x;
+        double[] temp=a[r1];
+        a[r1]=a[r2];
+        a[r2]=temp;
+        Matrix tm=new Matrix(a);
+        return tm;
+    }
+
+    public Matrix t2(int r, double a){
+        r-=1;
+        Matrix tm=new Matrix(this.x);
+        for(int i=0;i<this.cols;i++){
+               tm.x[r][i]=tm.x[r][i]*a;
+            }
+        return tm;
+    }
+
+    public Matrix t3(int r1, double a, int r2){
+        r1-=1;
+        r2-=1;
+        Matrix tm=new Matrix(this.x);
+        for(int i=0;i<this.cols;i++){
+            tm.x[r2][i]+=(tm.x[r1][i]*a);
+        }
+        return tm;
+    }
+
+    private double dot(double[] a, double[] b){
+        double x=0;
+        for(int i=0;i<a.length;i++){
+            x+=a[i]*b[i];
+        }
+        return x;
+    }
+
+    private Matrix scalarmult(double a){
+        Matrix n=new Matrix(x);
+        for(int i=0;i<this.rows;i++){
+            for(int j=0;j<this.cols;j++){
+               n.x[i][j]*=a;
+            }
+        }
+        return n;
+    }
+
+}
